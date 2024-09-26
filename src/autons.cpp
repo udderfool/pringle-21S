1bf6c3327225956b1191b7df5061ba6d25a23911
Qlbbl1111 <quinnlburke@icloud.com>
2024-09-26 14:36:33 -0700

    update 4ring

--- src/autons.cpp
+++ src/autons.cpp
@@ -103,26 +103,29 @@ void red_50WP(){
 void red_4ring(){
   //score on allaince stake
   chassis.pid_drive_set(-18.5_in, DRIVE_SPEED, false);
+  chassis.pid_wait_until(-8_in);
   chassis.pid_wait();
   chassis.pid_turn_set(90_deg, TURN_SPEED);
   chassis.pid_wait();
   chassis.pid_drive_set(-12_in, DRIVE_SPEED, false);
   intake.move(127);
-  pros::delay(500);
-  chassis.pid_wait_quick_chain();
+  chassis.pid_wait();
+  pros::delay(150);
   chassis.pid_drive_set(16_in, DRIVE_SPEED, true);
   chassis.pid_wait();
-  chassis.pid_turn_set(-135_deg, TURN_SPEED); //turn to mogo
+  chassis.pid_turn_set(225_deg, TURN_SPEED); //turn to mogo -135
   chassis.pid_wait();
   chassis.pid_drive_set(-35_in, 75, true);
   chassis.pid_wait_until(-32.75_in);
   mogomech.set(true);
   chassis.pid_wait();
   chassis.pid_turn_set(0_deg, TURN_SPEED); //turn to mogo
-  chassis.pid_wait_quick_chain();
-  chassis.pid_drive_set(30_in, DRIVE_SPEED, true);
-  pros::delay(500);
-  chassis.pid_drive_set(-2_in, DRIVE_SPEED, false);
+  chassis.pid_wait();
+  chassis.pid_drive_set(28_in, DRIVE_SPEED, true);
+  chassis.pid_wait_until(20_in);
+  intake.move(127);
+  chassis.pid_wait();
+  chassis.pid_drive_set(-1_in, DRIVE_SPEED, false);
   chassis.pid_wait();
   chassis.pid_turn_set(90_deg, TURN_SPEED);
   chassis.pid_wait_quick_chain();
@@ -132,17 +135,17 @@ void red_4ring(){
   chassis.pid_wait_quick_chain();
   chassis.pid_turn_set(0_deg, TURN_SPEED);
   chassis.pid_wait_quick_chain();
-  chassis.pid_drive_set(-9_in, DRIVE_SPEED, false);
+  chassis.pid_drive_set(-10_in, DRIVE_SPEED, false);
   chassis.pid_wait_quick_chain();
   chassis.pid_turn_set(90_deg, TURN_SPEED);
   chassis.pid_wait_quick_chain();
   chassis.pid_drive_set(8.5_in, DRIVE_SPEED, false);
   chassis.pid_wait_quick_chain();
-  chassis.pid_drive_set(-12_in, DRIVE_SPEED, true);
+  chassis.pid_drive_set(-1_in, DRIVE_SPEED, true);
   chassis.pid_wait_quick_chain();
   chassis.pid_turn_set(180_deg, TURN_SPEED);
   chassis.pid_wait_quick_chain();
-  chassis.pid_drive_set(36_in, DRIVE_SPEED, true);
+  chassis.pid_drive_set(30_in, DRIVE_SPEED, true);
   chassis.pid_wait();
 }
 
