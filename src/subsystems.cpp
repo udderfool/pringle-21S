ef8f1a23e6224de8f326886db3d23ea189bd3130
Qlbbl1111 <quinnlburke@icloud.com>
2024-10-15 09:36:52 -0700

    Update subsystems.cpp

--- src/subsystems.cpp
+++ src/subsystems.cpp
@@ -1,18 +1,10 @@
-#include "subsystems.hpp"
 #include "main.h"
 #include "pros/misc.h"
 
-using pros::E_CONTROLLER_DIGITAL_B;
-using pros::E_CONTROLLER_DIGITAL_DOWN;
-using pros::E_CONTROLLER_DIGITAL_L1;
-using pros::E_CONTROLLER_DIGITAL_L2;
-using pros::E_CONTROLLER_DIGITAL_R1;
-using pros::E_CONTROLLER_DIGITAL_R2;
-
 void setIntake() {
-  if (master.get_digital(E_CONTROLLER_DIGITAL_L2)) {
+  if (master.get_digital(pros::E_CONTROLLER_DIGITAL_L2)) {
     intake.move(127);
-  } else if (master.get_digital(E_CONTROLLER_DIGITAL_L1)) {
+  } else if (master.get_digital(pros::E_CONTROLLER_DIGITAL_L1)) {
     intake.move(-127);
   } else {
     intake.move(0);
@@ -20,11 +12,11 @@ void setIntake() {
 }
 
 void setMogo() {
-    mogomech.button_toggle(master.get_digital(E_CONTROLLER_DIGITAL_B));
+    mogomech.button_toggle(master.get_digital(pros::E_CONTROLLER_DIGITAL_B));
 }
 
 void setWall() {
-  if (master.get_digital(E_CONTROLLER_DIGITAL_DOWN)) {
+  if (master.get_digital(pros::E_CONTROLLER_DIGITAL_DOWN)) {
     wallmech.set(true);
   } else {
     wallmech.set(false);
@@ -33,16 +25,16 @@ void setWall() {
 
 void setClamp() {
   //if trigger button and optical sensor (red or blue) are true, then set clamp after [x] ms
-  while (master.get_digital(E_CONTROLLER_DIGITAL_R2)) {
+  while (master.get_digital(pros::E_CONTROLLER_DIGITAL_R2)) {
     if (ringsens.get_hue() < 240) {
       ringclamp.set(true);
     }
   }
-  if (!master.get_digital(E_CONTROLLER_DIGITAL_R2)) {
+  if (!master.get_digital(pros::E_CONTROLLER_DIGITAL_R2)) {
     ringclamp.set(false);
   }
 }
 
 bool shift() { 
-  return master.get_digital(E_CONTROLLER_DIGITAL_R1);
+  return master.get_digital(pros::E_CONTROLLER_DIGITAL_R1);
 }
