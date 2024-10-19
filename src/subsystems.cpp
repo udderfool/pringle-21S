ac4c64bea83b9d72421f276049211b4f389c9d08
shastalebab <shastalebab.hfas@gmail.com>
2024-10-18 22:52:11 -0700

    some control fixes

--- src/subsystems.cpp
+++ src/subsystems.cpp
@@ -2,9 +2,9 @@
 #include "pros/misc.h"
 
 void setIntake() {
-  if (master.get_digital(pros::E_CONTROLLER_DIGITAL_L2)) {
+  if (master.get_digital(pros::E_CONTROLLER_DIGITAL_L1)) {
     intake.move(127);
-  } else if (master.get_digital(pros::E_CONTROLLER_DIGITAL_L1)) {
+  } else if (master.get_digital(pros::E_CONTROLLER_DIGITAL_L2)) {
     intake.move(-127);
   } else {
     intake.move(0);
@@ -25,13 +25,16 @@ void setWall() {
 //temp code for test or something idk
 void setClamp() {
   //if trigger button and optical sensor (red or blue) are true, then set clamp after [x] ms
-  while (master.get_digital(pros::E_CONTROLLER_DIGITAL_R2)) {
+  /*while (master.get_digital(pros::E_CONTROLLER_DIGITAL_R2)) {
     if (ringsens.get_hue() < 240) {
       ringclamp.set(true);
     }
+  }*/
+  if (master.get_digital(pros::E_CONTROLLER_DIGITAL_R2)) {
+    ringclamp.set(false);
   }
   if (!master.get_digital(pros::E_CONTROLLER_DIGITAL_R2)) {
-    ringclamp.set(false);
+    ringclamp.set(true);
   }
 }
 
